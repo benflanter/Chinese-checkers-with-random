1bb6dcf7f2dbcc2d2b326d125f3daab953593170
benflanter <119852127+benflanter@users.noreply.github.com>
2023-01-07 16:53:04 +0200

    Add files via upload

---
+++ ChineseCheckers/ChineseCheckers/Model/ComputerPlayer.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChineseCheckers.Model
+{
+    class ComputerPlayer : Player
+    {
+        private Random random = new Random();
+        private byte[,] helpmat =
+        {
+            { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 }
+        };
+
+        public ComputerPlayer(bool side, Board board) :base(side, board)
+		{
+        }
+
+        public List<Move>  GetMoves()
+        {
+            List<Move> moves = new List<Move>();
+            foreach(KeyValuePair<int, Piece> piece in pieces)
+            {
+                List<Move> nearMoves = GetNearMoves(piece.Value);
+                List<Move> farMoves = GetFarMoves(piece.Value);
+                if(nearMoves!=null)
+                    moves.AddRange(nearMoves);
+                if (farMoves!=null)
+                moves.AddRange(farMoves);
+            }
+			return moves;
+		}
+
+        private List<Move> GetNearMoves(Piece piece)
+        {
+            List<Move> moves = new List<Move>();
+            for (int i = 0; i < board.directions.Length / 2; i++)
+            {
+                int row = piece.row + board.directions[i, 0];
+                int col = piece.col + board.directions[i, 1];
+                if (row >= 0 && row < Board.HEIGHT && col >= 0 && col < Board.WIDTH)
+                {
+                    if (Board.initmat[row, col] != 0 && board.getPiece(row, col) == null)
+                        moves.Add(new Move(piece, row, col));
+                }
+            }
+            return moves;
+        }
+
+        private List<Move> GetFarMoves(Piece piece)
+        {
+            List<Move> moves = new List<Move>();
+            if (GetNearMoves(piece).Count == 0)
+                return null;
+            ClearHelpMat();
+            ScanFarMoves(piece, piece, moves);
+            return moves;
+        }
+
+        private void ScanFarMoves(Piece myPiece, Piece nextPiece, List<Move> moves)
+        {
+            if (GetNearMoves(nextPiece) == null || helpmat[nextPiece.row,nextPiece.col] == 1 || Board.initmat[nextPiece.row, nextPiece.col] == 0)
+                return;
+            helpmat[nextPiece.row, nextPiece.col] = 1;
+            for (int i = 0; i < board.directions.Length / 2; i++)
+            {
+                int row = nextPiece.row + board.directions[i, 0];
+                int col = nextPiece.col + board.directions[i, 1];
+                if (Islegal(row, col) && board.getPiece(row, col) != null)
+                {
+                    int nextRow = row + board.directions[i, 0];
+                    int nextCol = col + board.directions[i, 1];
+                    if (Islegal(nextRow, nextCol) && board.getPiece(nextRow, nextCol) == null)
+                    {
+                        nextPiece = new Piece(nextRow, nextCol, myPiece.side);
+                        if(board.MoveAble(myPiece,nextRow,nextCol))
+                            moves.Add(new Move(myPiece, nextRow, nextCol));
+                        ScanFarMoves(myPiece, nextPiece, moves);
+                    }
+                }
+            }
+
+        }
+
+        private void ClearHelpMat()
+        {
+            for (int i = 0; i < Board.HEIGHT; i++)
+            {
+                for (int j = 0; j < Board.WIDTH; j++)
+                {
+                    helpmat[i, j] = 0;
+                }
+            }
+        }
+       private bool Islegal(int row, int col)
+        {
+            return row >= 0 && row < Board.HEIGHT && col >= 0 && col < Board.WIDTH && Board.initmat[row, col] != 0;
+        }
+
+        public void MakeMove()
+        {
+            
+            List<Move> moves = GetMoves();
+            int index = random.Next(0, moves.Count);
+            if (moves.Count > 0)
+            {
+                removePiece(moves[index].GetOrigin());
+                addPiece(moves[index].GetRow(), moves[index].GetCol(), side);
+            }
+        }
+    }
+}
---
+++ ChineseCheckers/ChineseCheckers/Model/Player.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace ChineseCheckers
+{
+	public class Player
+	{
+        protected Dictionary<int, Piece> pieces;
+	    public bool side;
+		protected Board board;
+
+		public Player(bool side, Board board)
+		{
+			this.board = board;
+			this.side = side;
+			pieces = new Dictionary<int, Piece>();
+			if (side) // up side
+            {
+				for (int i = 0; i < 4; i++)
+					for (int j = 0; j < Board.WIDTH; j++)
+					{
+						if (Board.initmat[i, j] == 2)
+							pieces.Add(i * Board.WIDTH + j, new Piece(i, j, side));
+					}
+			}
+            else // down side
+            {
+				for (int i = Board.HEIGHT - 4; i < Board.HEIGHT; i++)
+					for (int j = 0; j < Board.WIDTH; j++)
+					{
+						if (Board.initmat[i, j] == 3)
+							pieces.Add(i * Board.WIDTH + j, new Piece(i, j, side));
+					}
+			}
+		}
+
+		public void Draw(Graphics graphics)
+		{
+			foreach (Piece piece in pieces.Values)
+				piece.Draw(graphics);
+		}
+		public Piece getPiece(int row, int col)
+		{
+			int key = row * Board.WIDTH + col;
+			if (!pieces.ContainsKey(key))
+				return null;
+			return pieces[key];
+		}
+		public void removePiece(Piece piece)
+		{
+			int key = piece.row * Board.WIDTH + piece.col;
+			pieces.Remove(key);
+		}
+		public void addPiece(int rowDest, int colDest, bool side)
+		{
+			int key = rowDest * Board.WIDTH + colDest;
+			pieces.Add(key, new Piece(rowDest, colDest, side));
+		}
+
+		public bool CheckPlayerWin()
+		{
+			Piece piece;
+			if (this.side)
+			{	//player is up, checking down
+				for (int i = Board.HEIGHT - 4; i < Board.HEIGHT; i++)
+				{
+					for (int j = 0; j < Board.WIDTH; j++)
+					{
+						if (Board.initmat[i, j] == 3)
+						{
+							piece = getPiece(i, j);
+							if (piece == null || piece.side != this.side)
+								return false;
+						}
+					}
+				}
+				return true;
+			}
+			else
+            {   //player is down, checking up
+                for (int i = 0; i < 4; i++)
+				{
+					for (int j = 0; j < Board.WIDTH; j++)
+					{
+                        if(Board.initmat[i, j] == 2)
+                        {
+                            piece = getPiece(i, j);
+                            if (piece == null|| piece.side != this.side)
+                                return false;
+                        }
+                    }
+				}
+				return true;
+            }
+		}
+	}
+}
